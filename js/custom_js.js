651710e6e8cce143e9b1aefce229289dcf42a83e
cbhallamudi <36947619+cbhallamudi@users.noreply.github.com>
2021-07-28 11:23:25 +0530

    change in close tab and open tab function

--- js/custom_js.js
+++ js/custom_js.js
@@ -4,8 +4,8 @@ function addNewTab(){
   // _tabLength = document.getElementsByClassName('tabcontent').length;
   // console.log(_tabLength);
   // _tabLength = 1;
-  document.getElementById('tab').innerHTML += '<button class="tablinks" id="btnId'+_tabLength+'" onclick="openTab(this.id, \'tab'+_tabLength+'\')">Tab'+(_tabLength+1)+'<strong class="tab-close" onclick="closeThisTab('+_tabLength+')">&times;</strong></button>';
-  document.getElementById('middle-section').innerHTML += '<div id="tab'+_tabLength+'" class="tabcontent"><p>Tab number'+(_tabLength+1)+' Add Widgets here.</p></div>';
+  document.getElementById('tab').innerHTML += '<button class="tablinks" id="btnId'+_tabLength+'" onclick="openTab(this.id, \'tab'+_tabLength+'\')">Tab'+(_tabLength+1)+'</button>';
+  document.getElementById('middle-section').innerHTML += '<div id="tab'+_tabLength+'" class="tabcontent"><p>Tab number'+(_tabLength+1)+' Add Widgets here.</p><button class="tab-content-close-btn" onclick="closeThisTab('+_tabLength+')"><strong class="tab-close">&times;</strong></button></div>';
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -32,24 +32,31 @@ function openTab(btnId, tabId) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById("btnId").style.display = "block";
-  document.getElementById("tabId").className += " active";
+  document.getElementById(tabId).style.display = "block";
+  document.getElementById(btnId).className += " active";
 } 
 
 function closeThisTab(btnTabId){
   document.getElementById('tab'+btnTabId).remove();
   document.getElementById('btnId'+btnTabId).remove();
   var i, tabcontent, tablinks;
-  _tabLength = document.getElementsByClassName('tabcontent').length;
+  // _tabLength = document.getElementsByClassName('tabcontent').length;
   tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+  if(tabcontent.length > 1){
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById("middle-section").lastChild.style.display = "block";
+    document.getElementById("tab").lastChild.className += " active";
+    // _tabLength++;
+  }else{
+    document.getElementById("tab0").style.display = "block";
+    document.getElementById("btnId0").className += " active";
+    // _tabLength++;   
   }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById("middle-section").lastChild.style.display = "block";
-  document.getElementById("tab").lastChild.className += " active";
-  _tabLength++;
+
 }
