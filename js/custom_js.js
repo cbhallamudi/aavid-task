7f92e5a909990ca9034fee5dfba0cea47bf5414d
cbhallamudi <36947619+cbhallamudi@users.noreply.github.com>
2021-07-31 02:39:03 +0530

    Final Changes

--- js/custom_js.js
+++ js/custom_js.js
@@ -1,13 +1,13 @@
 window.onload = function() {
-  initDragElement(element="");
+  initDragElement();
 };
 // Draggable Divs
-function initDragElement(element) {
-  if(element == ""){
-    console.log("No Element");
-  }else{
+function initDragElement() {
+  // if(element == ""){
+  //   console.log("No Element");
+  // }else{
 
-  console.log(element.parentElement.id);
+  // console.log(element.parentElement.id);
   // console.log("Dragging");
   var pos1 = 0,
     pos2 = 0,
@@ -72,13 +72,14 @@ function initDragElement(element) {
     var headerItems = element.getElementsByClassName("popup-header");
 
     if (headerItems.length == 1) {
+      console.log(headerItems[0])
       return headerItems[0];
     }
 
     return null;
   }
   }
-}
+// }
 
 
 // ================================================================
@@ -97,7 +98,7 @@ function addNewTab(){
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
   document.getElementById('tab').innerHTML += '<button class="tablinks" id="btnId'+_tabLength+'" onclick="openTab(this.id, \'tab'+_tabLength+'\')">Tab'+(_tabLength+1)+'</button>';
-  document.getElementById('middle-section').innerHTML += '<div id="tab'+_tabLength+'" class="tabcontent"><p>New Tab, Add Widgets here.</p><button class="tab-content-close-btn" onclick="closeThisTab('+_tabLength+')"><strong class="tab-close">&times;</strong></button></div>';
+  document.getElementById('middle-section').innerHTML += '<div id="tab'+_tabLength+'" class="tabcontent"><button class="tab-content-close-btn" onclick="closeThisTab('+_tabLength+')"><strong class="tab-close">&times;</strong></button></div>';
   document.getElementById('tab'+_tabLength).style.display = "block";
   document.getElementById('tab'+_tabLength).className += " currentTab";
   document.getElementById('btnId'+_tabLength).className += " active";
@@ -159,21 +160,35 @@ function addWidgetDropdown() {
 
 function addToCurrentTab(widgetClass) {
 
-  console.log(widgetClass.split("-")[0]);
+  // console.log(widgetClass.split("-")[0]);
+  var widgetName = widgetClass.split("-")[0];
+  if(widgetName == "quote"){
+    var dummyContent = "A stock quote is the price of a stock as quoted on an exchange. A basic quote for a specific stock provides information, such as its bid and ask price, last traded price, and volume traded.";
+  }else if(widgetName == "order"){
+    var dummyContent = "An order is an instruction that an investor gives to buy or sell stocks on a trading platform or to a stock broker.";
 
+  }else if(widgetName == "chart"){
+    var dummyContent = "A graphical representation of price and volume movements of a stock over a certain period of time.";
+    
+  }else{
+    var dummyContent = "A position is the amount of a security, asset, or property that is owned (or sold short) by some individual or other entity.";
+    
+  }
   var i, tabcontent;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     if(tabcontent[i].classList.contains("currentTab")){
       // console.log(tabcontent[i].getAttribute("id"));
-      tabcontent[i].innerHTML += '<div onmousedown="initDragElement(this)" class="popup '+widgetClass+'" id="'+tabcontent[i].getAttribute("id")+'-'+widgetClass+'"><div class="popup-header">Draggable and Resizable Widget: Quote</div><p>A stock quote is the price of a stock as quoted on an exchange. A basic quote for a specific stock provides information, such as its bid and ask price, last traded price, and volume traded.</p><div class="popup-footer"><button onclick="closeWidget(\''+tabcontent[i].getAttribute("id")+'-'+widgetClass+'\')">Close Widget &times;</button></div></div>';
+      tabcontent[i].innerHTML += '<div onmousedown="initDragElement()" class="popup '+widgetClass+'" id="'+tabcontent[i].getAttribute("id")+'-'+widgetClass+'"><div class="popup-header">'+widgetName+'</div><p>'+dummyContent+'</p><div class="popup-footer"><button onclick="closeWidget(\''+tabcontent[i].getAttribute("id")+'-'+widgetClass+'\')">Close Widget &times;</button></div></div>';
     }
   }
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-  // element.classList.contains(class);
+function openProfileDiv() {
+  document.getElementById("profile").style.display = "block";
+}
 
-
-
-
-
+function closeProfileDiv() {
+  document.getElementById("profile").style.display = "none";
 }
