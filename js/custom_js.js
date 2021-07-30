734ca8721a077ace258bbb86b4f89ae41c71ea1c
cbhallamudi <36947619+cbhallamudi@users.noreply.github.com>
2021-07-30 09:02:10 +0530

    add widgets to current tab

--- js/custom_js.js
+++ js/custom_js.js
@@ -1,21 +1,21 @@
 var _tabLength = 1;
 
 function addNewTab(){
-  // _tabLength = document.getElementsByClassName('tabcontent').length;
-  // console.log(_tabLength);
-  // _tabLength = 1;
-  document.getElementById('tab').innerHTML += '<button class="tablinks" id="btnId'+_tabLength+'" onclick="openTab(this.id, \'tab'+_tabLength+'\')">Tab'+(_tabLength+1)+'</button>';
-  document.getElementById('middle-section').innerHTML += '<div id="tab'+_tabLength+'" class="tabcontent"><p>New Tab, Add Widgets here.</p><button class="tab-content-close-btn" onclick="closeThisTab('+_tabLength+')"><strong class="tab-close">&times;</strong></button></div>';
+  
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
+    tabcontent[i].classList.remove("currentTab");
   }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
+  document.getElementById('tab').innerHTML += '<button class="tablinks" id="btnId'+_tabLength+'" onclick="openTab(this.id, \'tab'+_tabLength+'\')">Tab'+(_tabLength+1)+'</button>';
+  document.getElementById('middle-section').innerHTML += '<div id="tab'+_tabLength+'" class="tabcontent"><p>New Tab, Add Widgets here.</p><button class="tab-content-close-btn" onclick="closeThisTab('+_tabLength+')"><strong class="tab-close">&times;</strong></button></div>';
   document.getElementById('tab'+_tabLength).style.display = "block";
+  document.getElementById('tab'+_tabLength).className += " currentTab";
   document.getElementById('btnId'+_tabLength).className += " active";
   _tabLength++;
   console.log(_tabLength);
@@ -27,12 +27,14 @@ function openTab(btnId, tabId) {
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
+    tabcontent[i].classList.remove("currentTab");
   }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
   document.getElementById(tabId).style.display = "block";
+  document.getElementById(tabId).className += " currentTab";
   document.getElementById(btnId).className += " active";
 } 
 
@@ -60,138 +62,31 @@ function closeThisTab(btnTabId){
   }
 
 }
-//  Draggable Widgets
 
-window.onload = function() {
-  initDragElement();
-  initResizeElement();
-};
-
-function initDragElement() {
-  var pos1 = 0,
-    pos2 = 0,
-    pos3 = 0,
-    pos4 = 0;
-  var popups = document.getElementsByClassName("popup");
-  var elmnt = null;
-  var currentZIndex = 100; //TODO reset z index when a threshold is passed
-
-  for (var i = 0; i < popups.length; i++) {
-    var popup = popups[i];
-    var header = getHeader(popup);
-
-    popup.onmousedown = function() {
-      this.style.zIndex = "" + ++currentZIndex;
-    };
-
-    if (header) {
-      header.parentPopup = popup;
-      header.onmousedown = dragMouseDown;
-    }
-  }
-
-  function dragMouseDown(e) {
-    elmnt = this.parentPopup;
-    elmnt.style.zIndex = "" + ++currentZIndex;
-
-    e = e || window.event;
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    if (!elmnt) {
-      return;
-    }
-
-    e = e || window.event;
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-
-  function getHeader(element) {
-    var headerItems = element.getElementsByClassName("popup-header");
-
-    if (headerItems.length === 1) {
-      return headerItems[0];
-    }
-
-    return null;
-  }
-}
-
-function initResizeElement() {
-  var popups = document.getElementsByClassName("popup");
-  var element = null;
-  var startX, startY, startWidth, startHeight;
-
-  for (var i = 0; i < popups.length; i++) {
-    var p = popups[i];
-
-    var right = document.createElement("div");
-    right.className = "resizer-right";
-    p.appendChild(right);
-    right.addEventListener("mousedown", initDrag, false);
-    right.parentPopup = p;
-
-    var bottom = document.createElement("div");
-    bottom.className = "resizer-bottom";
-    p.appendChild(bottom);
-    bottom.addEventListener("mousedown", initDrag, false);
-    bottom.parentPopup = p;
-
-    var both = document.createElement("div");
-    both.className = "resizer-both";
-    p.appendChild(both);
-    both.addEventListener("mousedown", initDrag, false);
-    both.parentPopup = p;
-  }
-
-  function initDrag(e) {
-    element = this.parentPopup;
-
-    startX = e.clientX;
-    startY = e.clientY;
-    startWidth = parseInt(
-      document.defaultView.getComputedStyle(element).width,
-      10
-    );
-    startHeight = parseInt(
-      document.defaultView.getComputedStyle(element).height,
-      10
-    );
-    document.documentElement.addEventListener("mousemove", doDrag, false);
-    document.documentElement.addEventListener("mouseup", stopDrag, false);
-  }
-
-  function doDrag(e) {
-    element.style.width = startWidth + e.clientX - startX + "px";
-    element.style.height = startHeight + e.clientY - startY + "px";
-  }
-
-  function stopDrag() {
-    document.documentElement.removeEventListener("mousemove", doDrag, false);
-    document.documentElement.removeEventListener("mouseup", stopDrag, false);
-  }
-}
 
 function closeWidget(widgetId){
     document.getElementById(widgetId).remove();
 }
 
+function addWidgetDropdown() {
+  console.log("function triggered");
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function addToCurrentTab(widgetClass) {
+
+  console.log(widgetClass.split("-")[0]);
+
+  var i, tabcontent;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    if(tabcontent[i].classList.contains("currentTab")){
+      // console.log(tabcontent[i].getAttribute("id"));
+      tabcontent[i].innerHTML += '<div class="popup '+widgetClass+'" id="'+tabcontent[i].getAttribute("id")+'-'+widgetClass+'"><div class="popup-header">Draggable and Resizable Widget: Quote</div><p>A stock quote is the price of a stock as quoted on an exchange. A basic quote for a specific stock provides information, such as its bid and ask price, last traded price, and volume traded.</p><div class="popup-footer"><button onclick="closeWidget(\''+tabcontent[i].getAttribute("id")+'-'+widgetClass+'\')">Close Widget &times;</button></div></div>';
+    }
+  }
+
+  // element.classList.contains(class);
+
+
+}
